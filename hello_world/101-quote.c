0b34f02707baa35fe73983067d144cec8dff5fc3
Nkurikiyimana Aimable <aimablenkurikiyimana1@gmail.com>
2025-09-05 20:43:39 +0200

    .

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,13 +1,14 @@
-#include <stdio.h>
 #include <unistd.h>
+#include <string.h>
+
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
 
-int main(void)
-{
-fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+int main() {
+const char *msg = "This is an error message\n";
+write(2, msg, strlen(msg)); // 2 = stderr
 return (1);
 }
