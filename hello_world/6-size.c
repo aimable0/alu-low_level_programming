7ad398a23a6cfe51d14e775516a20f7779d872da
Nkurikiyimana Aimable <aimablenkurikiyimana1@gmail.com>
2025-09-05 20:22:51 +0200

    conforming to betty styles

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -7,16 +7,16 @@
 
  int main(void)
  {
-    int a;
-    long int b;
-    long long int c;
-    char d;
-    float e;
-    printf("Size of a char: %zu byte(s)\n", sizeof d);
-    printf("Size of a int: %zu byte(s)\n", sizeof a);
-    printf("Size of a long int: %zu byte(s)\n", sizeof b);
-    printf("Size of a long long int: %zu byte(s)\n", sizeof c);
-    printf("Size of a float: %zu byte(s)\n", sizeof e);
+int a;
+long int b;
+long long int c;
+char d;
+float e;
+printf("Size of a char: %zu byte(s)\n", sizeof d);
+printf("Size of a int: %zu byte(s)\n", sizeof a);
+printf("Size of a long int: %zu byte(s)\n", sizeof b);
+printf("Size of a long long int: %zu byte(s)\n", sizeof c);
+printf("Size of a float: %zu byte(s)\n", sizeof e);
 
-    return (0);
+return (0);
  }
