9d8e6f60b4070566a5a173ac7a89655e68608fb8
Nkurikiyimana Aimable <aimablenkurikiyimana1@gmail.com>
2025-09-15 21:05:23 +0200

    .

---
+++ variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+    int i, m;
+
+    for (i = 48; i < (48 + 10); i++)
+    {
+        for (m = 48 + (i - 48); m < 58; m++)
+        {
+            if (i != m) {
+                putchar(i);
+                putchar(m);
+                putchar(',');
+                putchar(' ');
+            }
+
+        }
+    }
+
+    return (0);
+}
