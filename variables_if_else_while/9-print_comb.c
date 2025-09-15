9692dc00c8fb8f2cb5c699ec97bb8e34fb476f60
Nkurikiyimana Aimable <aimablenkurikiyimana1@gmail.com>
2025-09-15 20:43:37 +0200

    .

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -5,17 +5,18 @@
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 int i;
-for (i = 48; i < (48 + 10); i++)
+for(i = 48; i < (48 + 10); i++)
 {
 putchar(i);
 if ( i != 48 + 9)
 {
 putchar(',');
-putchar(' ');
 }
+putchar(' ');
 }
 
 return (0);
