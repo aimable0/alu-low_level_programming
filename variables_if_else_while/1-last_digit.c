22a139c7da3c995924718f94219da86909f9fe4b
Nkurikiyimana Aimable <aimablenkurikiyimana1@gmail.com>
2025-09-15 19:48:43 +0200

    .

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -2,7 +2,11 @@
 #include <time.h>
 #include <stdio.h>
 
-/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 int n;
@@ -14,15 +18,15 @@ n = rand() - RAND_MAX / 2;
 printf("Last digit of %d is %d ", n, (n % 10));
 if (n < 5)
 {
-    printf("and is greater than 5\n");
+printf("and is greater than 5\n");
 }
 else if (n == 0)
 {
-    printf("and is greater than 5\n");
+printf("and is greater than 5\n");
 }
 else if (n > 6)
 {
-    printf("and is less than 6 and not 0\n");
+printf("and is less than 6 and not 0\n");
 }
 
 return (0);
