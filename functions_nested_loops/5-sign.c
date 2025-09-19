3ffa3bd1d0d4578ff8fa4deff3a4851944c089f2
Nkurikiyimana Aimable <aimablenkurikiyimana1@gmail.com>
2025-09-19 20:41:14 +0200

    .

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -3,29 +3,29 @@
 
 /**
  * print_sign - checks for sign and prints sign.
- * @c: the character to check
+ * @n: the character to check
  *
  * Return: 1 if c is positve
  * and 0 if c is zero.
  * and -1 if c is negative.
  */
 
-int print_sign(int c)
+int print_sign(int n)
 {
 if (c > 0)
 {
 _putchar('+');
 return (1);
 }
-else if (c == 0)
-{
-_putchar('0');
-return (0);
-}
 else if (c < 0)
 {
 _putchar('-');
 return (-1);
+}
+else
+{
+_putchar('0');
+return (0);
 }
 
 }
