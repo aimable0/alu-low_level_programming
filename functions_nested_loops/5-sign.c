bcab49ddbb37f37d6e0c55c0c497a23346b571c6
Nkurikiyimana Aimable <aimablenkurikiyimana1@gmail.com>
2025-09-19 20:38:49 +0200

    .

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_sign - checks for sign and prints sign.
+ * @c: the character to check
+ *
+ * Return: 1 if c is positve
+ * and 0 if c is zero.
+ * and -1 if c is negative.
+ */
+
+int print_sign(int c)
+{
+if (c > 0)
+{
+_putchar('+');
+return (1);
+}
+
+if (c == 0)
+{
+_putchar('0');
+return (0);
+}
+
+if (c < 0)
+{
+_putchar('-');
+return (-1);
+}
+
+}
