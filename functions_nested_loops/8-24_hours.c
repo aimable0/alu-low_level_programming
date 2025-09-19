bb60d52122556e094a0ed3ee04aa2def2a4d05a4
Nkurikiyimana Aimable <aimablenkurikiyimana1@gmail.com>
2025-09-19 23:37:02 +0200

    .

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -15,34 +15,34 @@ int min = 0;
 int hr = 0;
 
 for (hr = 0; hr < 24; hr++) {
-    for (min = 0; min < 60; min++) {
+for (min = 0; min < 60; min++) {
 
-        /* handling hours */
-        if (hr < 10) {
-            _putchar('0');
-            _putchar(hr + '0');
-        } else {
-            /* displaying hours with 2 digits */
-            int last_digit = hr % 10;
-            int first_digit = (hr - last_digit) / 10;
-            _putchar(first_digit + '0');
-            _putchar(last_digit + '0');
-        }
+/* handling hours */
+if (hr < 10) {
+_putchar('0');
+_putchar(hr + '0');
+} else {
+/* displaying hours with 2 digits */
+int last_digit = hr % 10;
+int first_digit = (hr - last_digit) / 10;
+_putchar(first_digit + '0');
+_putchar(last_digit + '0');
+}
 
-        _putchar(':');
+_putchar(':');
 
-        /* handling minutes */
-        if (min < 10) {
-            _putchar('0');
-            _putchar(min + '0');
-        } else {
-            /* displaying minutes with 2 digits */
-            int last_num = min % 10;
-            int first_num = (min - last_num) / 10;
-            _putchar(first_num + '0');
-            _putchar(last_num + '0');
-        }
-        _putchar('\n');
-    }
+/* handling minutes */
+if (min < 10) {
+_putchar('0');
+_putchar(min + '0');
+} else {
+/* displaying minutes with 2 digits */
+int last_num = min % 10;
+int first_num = (min - last_num) / 10;
+_putchar(first_num + '0');
+_putchar(last_num + '0');
+}
+_putchar('\n');
+}
 }
 }
