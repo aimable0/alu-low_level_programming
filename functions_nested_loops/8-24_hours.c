f83d34a03cf684327ad4a10a553303069db1764d
Nkurikiyimana Aimable <aimablenkurikiyimana1@gmail.com>
2025-09-20 00:02:21 +0200

    .

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -5,35 +5,33 @@
  *
  * Return: void always
  */
+void _print_time(time);
+
 void jack_bauer(void)
 {
-int hr, min;
-for (hr = 0; hr < 24; hr++)
-{
-for (min = 0; min < 60; min++)
-{
-if (hr < 10)
-{
-_putchar('0');
-_putchar(hr + '0');
+    int hr, min;
+    for (hr = 0; hr < 24; hr++)
+    {
+        for (min = 0; min < 60; min++)
+        {
+            _print_time(hr);
+            _putchar(':');
+            _print_time(min);
+            _putchar('\n');
+        }
+    }
 }
-else
+
+void _print_time(time)
 {
-_putchar(((hr - (hr % 10)) / 10) + '0');
-_putchar((hr % 10) + '0');
-}
-_putchar(':');
-if (min < 10)
-{
-_putchar('0');
-_putchar(min + '0');
-}
-else
-{
-_putchar(((min - (min % 10)) / 10) + '0');
-_putchar((min % 10) + '0');
-}
-_putchar('\n');
-}
-}
+    if (time < 10)
+    {
+        _putchar('0');
+        _putchar(time + '0');
+    }
+    else
+    {
+        _putchar(((time - (time % 10)) / 10) + '0');
+        _putchar((time % 10) + '0');
+    }
 }
