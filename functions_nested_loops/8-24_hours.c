f1f5019def0cf27d71b8feb4cc9f764031882880
Nkurikiyimana Aimable <aimablenkurikiyimana1@gmail.com>
2025-09-19 23:37:57 +0200

    styling it the betty way

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -8,20 +8,24 @@
  * Return: void always
  */
 
-
 void jack_bauer(void)
 {
-int min = 0;
-int hr = 0;
+    int min = 0;
+    int hr = 0;
 
-for (hr = 0; hr < 24; hr++) {
-for (min = 0; min < 60; min++) {
+    for (hr = 0; hr < 24; hr++)
+    {
+for (min = 0; min < 60; min++)
+{
 
 /* handling hours */
-if (hr < 10) {
+if (hr < 10)
+{
 _putchar('0');
 _putchar(hr + '0');
-} else {
+}
+else
+{
 /* displaying hours with 2 digits */
 int last_digit = hr % 10;
 int first_digit = (hr - last_digit) / 10;
@@ -32,10 +36,13 @@ _putchar(last_digit + '0');
 _putchar(':');
 
 /* handling minutes */
-if (min < 10) {
+if (min < 10)
+{
 _putchar('0');
 _putchar(min + '0');
-} else {
+}
+else
+{
 /* displaying minutes with 2 digits */
 int last_num = min % 10;
 int first_num = (min - last_num) / 10;
@@ -44,5 +51,5 @@ _putchar(last_num + '0');
 }
 _putchar('\n');
 }
-}
+    }
 }
