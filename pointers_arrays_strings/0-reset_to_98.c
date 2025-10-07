6d621fc3c110def181327757b61e7f3961d6a50c
Nkurikiyimana Aimable <aimablenkurikiyimana1@gmail.com>
2025-10-07 11:18:27 +0200

    refining function description

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
- * void - resets n to 98
- *
+ * reset_to_98 - resets n to 98
+ * @n: variable whose value is to be changed to 98
  * Return: Always void
  */
 void reset_to_98(int *n)
