193ce23d873706a7ee372c2845c8c32232b8c2e3
Nkurikiyimana Aimable <aimablenkurikiyimana1@gmail.com>
2025-10-07 13:58:42 +0200

    fix oo1

--- pointers_arrays_strings/7-puts.half.c
+++ pointers_arrays_strings/7-puts.half.c
@@ -17,8 +17,7 @@ while (str[len] != '\0')
 len++;
 }
 
-i = (len % 2 == 0) ? len / 2 : (len - 1) / 2;
-for (; i < len ; i++)
+for (i = (len % 2 == 0) ? len / 2 : (len - 1) / 2; i < len ; i++)
 {
 _putchar(str[i]);
 }
