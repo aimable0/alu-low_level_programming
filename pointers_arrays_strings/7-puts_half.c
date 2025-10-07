f0bb9978bdb7f58232f75190dbd379eed06ae8f0
Nkurikiyimana Aimable <aimablenkurikiyimana1@gmail.com>
2025-10-07 15:15:14 +0200

    4

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -19,7 +19,8 @@ len++;
 }
 
 
-for (i = (len % 2 == 0) ? len / 2 : (len - 1) / 2; i < len ; i++)
+// for (i = (len % 2 == 0) ? len / 2 : (len - 1) / 2; i < len ; i++);
+for (i = len / 2; i < len ; i++)
 {
 c = str[i];
 write(1, &c, 1);
