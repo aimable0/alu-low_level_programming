1542c0f11b0ebc60b5e6b9ed120213416492f166
Nkurikiyimana Aimable <aimablenkurikiyimana1@gmail.com>
2025-10-07 17:05:08 +0200

    d

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -11,9 +11,13 @@ void print_array(int *a, int n)
 {
 int i = 0;
 
-for (i = 0; i < n - 1; i++)
+for (i = 0; i < n; i++)
 {
+if (i = n - 1 )
+{
+printf("%i\n", a[i]);
+break;
+}
 printf("%i, ", a[i]);
 }
-printf("%i\n", a[n - 1]);
 }
