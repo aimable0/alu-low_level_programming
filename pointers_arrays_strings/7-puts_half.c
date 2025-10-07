bcf269eb5d8e3908c6e2412e62e15efb793f132e
Nkurikiyimana Aimable <aimablenkurikiyimana1@gmail.com>
2025-10-07 15:06:18 +0200

    trying something

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -11,15 +11,18 @@ void puts_half(char *str)
 {
 int len = 0;
 int i = 0;
+char c;
 
 while (str[len] != '\0')
 {
 len++;
 }
 
+
 for (i = (len % 2 == 0) ? len / 2 : (len - 1) / 2; i < len ; i++)
 {
-write(1, &str[i], 1);
+c = str[i];
+write(1, &c, 1);
 }
 write(1, '\n', 1);
 }
