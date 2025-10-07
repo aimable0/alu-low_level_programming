1722cef5d72a541a6bf2df6e123d601634ad9971
Nkurikiyimana Aimable <aimablenkurikiyimana1@gmail.com>
2025-10-07 13:21:19 +0200

    betty

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -15,9 +15,13 @@ int len = 0;
 char temp;
 int i = 0;
 
-while (s[len] != '\0') len++;
+while (s[len] != '\0')
+{
+len++;
+}
 
-for (i = 0, len -= 1; i < (len + 1) / 2; i++) {
+for (i = 0, len -= 1; i < (len + 1) / 2; i++)
+{
 temp = s[i];
 s[i] = s[len - i];
 s[len - i] = temp;
