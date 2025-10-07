6925c3c4fdc7724f7aea05612016d34ee3d99025
Nkurikiyimana Aimable <aimablenkurikiyimana1@gmail.com>
2025-10-07 13:56:47 +0200

    .

---
+++ pointers_arrays_strings/7-puts.half.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * puts_half - prints second half of a string
+ * @str: string
+ * Return: Always void
+ */
+
+void puts_half(char *str)
+{
+int len = 0;
+int i = 0;
+
+while (str[len] != '\0')
+{
+len++;
+}
+
+i = (len % 2 == 0) ? len / 2 : (len - 1) / 2;
+for (; i < len ; i++)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
+}
