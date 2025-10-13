4338142cdb6cbbc11d58368adfb89dabec1cffa3
Nkurikiyimana Aimable <aimablenkurikiyimana1@gmail.com>
2025-10-13 15:07:36 +0200

    factorial.c

---
+++ recursion/3-factorial.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * factorial - calculates the length of a string
+ * @n: integer number
+ *
+ * Return: factorial of n (int) Always.
+ */
+
+int factorial(int n)
+{
+if (n < 0)
+{
+return (-1);
+}
+
+if (n == 0)
+{
+return 1;
+}
+return (n * factorial(n - 1));
+}
