a52356e906db71d4c8172fa9d3198a350e40b37f
Nkurikiyimana Aimable <aimablenkurikiyimana1@gmail.com>
2025-10-13 18:07:03 +0200

    .

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * _puts_recursion - prints a string, followed by a new line.
+ * _puts_rev_recursion - prints a string in reverse.
  * @s: pointer to the first character of the string
  *
  * Return: Void Always.
