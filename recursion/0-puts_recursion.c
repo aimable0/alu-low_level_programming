7b234c12981f68d04229d3c8b749b6aedd34525a
Nkurikiyimana Aimable <aimablenkurikiyimana1@gmail.com>
2025-10-13 13:52:49 +0200

    betty style

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
 #include "main.h"
 
+/**
+ * _puts_recursion - prints a string, followed by a new line.
+ * @s: pointer to the first character of the string
+ *
+ * Return: Void Always.
+ */
+
 void _puts_recursion(char *s)
 {
-if (*s == '\0') {
+if (*s == '\0')
+{
 _putchar('\n');
 }
 else
