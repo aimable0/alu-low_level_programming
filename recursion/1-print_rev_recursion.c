c65e50f22d07c40c8357f1981095448c2ea6ae04
Nkurikiyimana Aimable <aimablenkurikiyimana1@gmail.com>
2025-10-13 17:55:01 +0200

    reverse string

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-//!\ #include "main.h"
+#include "main.h"
 
 /**
  * _puts_recursion - prints a string, followed by a new line.
@@ -8,24 +8,11 @@
  * Return: Void Always.
  */
 
-void _print_rev_recursion(char *s);
-
-int main(void)
-{
-    _print_rev_recursion("\nColton Walker");
-
-    return 0;
-}
-
 void _print_rev_recursion(char *s)
 {
-    // le'ts try to manually get at the end of the string
-
-    int i = 0;
-    while (*(s + i) != '\0')
-        i++;
-
-    putchar(*s);
-    _print_rev_recursion(s + (i  - 1));
-    return;
+if (*s != '\0')
+{
+_print_rev_recursion(s + 1);
+putchar(*s);
+}
 }
