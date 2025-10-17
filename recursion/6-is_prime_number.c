95f0ded6388543e2cd7f1191deeb797c60010a0e
Nkurikiyimana Aimable <aimablenkurikiyimana1@gmail.com>
2025-10-17 19:11:01 +0200

    refined version

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -11,13 +11,14 @@
 
 int is_prime_number(int n)
 {
-    if (n <= 1)
-        return 0;
+int i = 0;
+if (n <= 1)
+return (0);
 
-    for (int i = 0; i < n; i++)
-    {
-        if (i != 1 && i != n && n % i == 0)
-            return 0;
-    }
-    return 1;
+for (i = 0; i < n; i++)
+{
+if (i != 1 && i != n && n % i == 0)
+return (0);
+}
+return (1);
 }
