0ce2948825edc73684e66d3b89865f001d69b365
Nkurikiyimana Aimable <aimablenkurikiyimana1@gmail.com>
2025-10-17 19:46:56 +0200

    ..

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 #include "main.h"
 
+/* Helper funciton */
+int check_prime(int n, int i)
+{
+    if (i != n && n % i == 0)
+        return 0;
+
+    if (i == n)
+        return 1;
+    else
+        return check_prime(n, i++);
+}
+
 /**
  * is_prime_number - detects if a number is a prime number.
  * @n: integer number to check for.
@@ -8,17 +20,13 @@
  * Return: 1 if prime and 0 otherwise.
  */
 
-
 int is_prime_number(int n)
 {
-int i = 0;
-if (n <= 1)
-return (0);
-
-for (i = 0; i < n; i++)
-{
-if (i != 1 && i != n && n % i == 0)
-return (0);
-}
-return (1);
+    int i = 0;
+    if (n <= 1)
+        return (0);
+    if (n == 2)
+        return (1);
+    else
+        return check_prime(n, 2);
 }
