e5e33fa3fc49e51336ea94a997b319b15b873a63
Nkurikiyimana Aimable <aimablenkurikiyimana1@gmail.com>
2025-10-17 18:57:57 +0200

    more c

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -28,27 +28,27 @@ int main(void)
 
 int is_prime_number(int n)
 {
-    if (n <= 1)
-    {
-        return 0;
-    }
-    if (n == 2)
-    {
-        return 1;
-    }
-    if (n % 2 == 0)
-    {
-        return 0;
-    }
+int i = 0;
+int limit = (int)sqrt((double)n);
+if (n <= 1)
+{
+return 0;
+}
+if (n == 2)
+{
+return 1;
+}
+if (n % 2 == 0)
+{
+return 0;
+}
 
-    int i = 0;
-    int limit = (int)sqrt((double)n);
-    for (i = 3; i <= limit; i += 2)
-    {
-        if (n % i == 0)
-        {
-            return (0);
-        }
-    }
-    return 1;
+for (i = 3; i <= limit; i += 2)
+{
+if (n % i == 0)
+{
+return (0);
+}
+}
+return 1;
 }
