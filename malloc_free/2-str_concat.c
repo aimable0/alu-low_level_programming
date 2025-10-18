992ed0628f1b3eb7bc951c2a92bbfa6fa0341ff1
Nkurikiyimana Aimable <aimablenkurikiyimana1@gmail.com>
2025-10-18 22:28:26 +0200

    010

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -13,27 +13,27 @@
  */
 char *concat_strings(char *concat, char *s1, char *s2, int len_s1, int len_s2)
 {
-    int i = 0;
-    int j = 0;
+int i = 0;
+int j = 0;
 
-    /* check */
-    if (concat == NULL)
-    {
-        return (NULL);
-    }
+/* check */
+if (concat == NULL)
+{
+return (NULL);
+}
 
-    for (i = 0; i < len_s1; i++)
-    {
-        concat[i] = s1[i];
-    }
+for (i = 0; i < len_s1; i++)
+{
+concat[i] = s1[i];
+}
 
-    for (i = len_s1; i < (len_s1 + len_s2); i++, j++)
-    {
-        concat[i] = s2[j];
-    }
+for (i = len_s1; i < (len_s1 + len_s2); i++, j++)
+{
+concat[i] = s2[j];
+}
 
-    concat[len_s1 + len_s2] = '\0';
-    return (concat);
+concat[len_s1 + len_s2] = '\0';
+return (concat);
 }
 
 /**
@@ -46,33 +46,33 @@ char *concat_strings(char *concat, char *s1, char *s2, int len_s1, int len_s2)
 
 char *str_concat(char *s1, char *s2)
 {
-    int i = 0;
-    char *concat;
-    int len_s1 = 0;
-    int len_s2 = 0;
+int i = 0;
+char *concat;
+int len_s1 = 0;
+int len_s2 = 0;
 
-    if (s1 != NULL) /* not very effecient logic */
-    {
-        while (s1[i] != '\0')
-        {
-            i++;
-            ++len_s1;
-        }
-    }
+if (s1 != NULL) /* not very effecient logic */
+{
+while (s1[i] != '\0')
+{
+i++;
+++len_s1;
+}
+}
 
-    if (s2 != NULL) /* not very effecient logic */
-    {
-        i = 0;
-        while (s2[i] != '\0')
-        {
-            i++;
-            ++len_s2;
-        }
-        ++len_s2;
-    }
+if (s2 != NULL) /* not very effecient logic */
+{
+i = 0;
+while (s2[i] != '\0')
+{
+i++;
+++len_s2;
+}
+++len_s2;
+}
 
-    /* create a new memory block */
-    concat = malloc(sizeof(char) * (len_s1 + len_s2));
-    concat = concat_strings(concat, s1, s2, len_s1, len_s2);
-    return (concat);
+/* create a new memory block */
+concat = malloc(sizeof(char) * (len_s1 + len_s2));
+concat = concat_strings(concat, s1, s2, len_s1, len_s2);
+return (concat);
 }
