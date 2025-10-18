281610687f27e9f8948e5805ea8bbfc2518cc7c9
Nkurikiyimana Aimable <aimablenkurikiyimana1@gmail.com>
2025-10-18 22:53:20 +0200

    last q

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -1,2 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+void free_grid(int **grid, int height)
+{
+    int i = 0;
+    for (i = 0; i < height; i++)
+    {
+        free(grid[i]);
+    }
+    free(grid);
+}
