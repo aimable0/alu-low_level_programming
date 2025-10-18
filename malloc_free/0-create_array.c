8379b2ac080707fe3ba50838d845029cfa05ca71
Nkurikiyimana Aimable <aimablenkurikiyimana1@gmail.com>
2025-10-18 21:08:40 +0200

    betty

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -4,12 +4,12 @@
 /**
  * create_array - creates an array of chars and initializes it with specif char
  * @c: letter to initalize with
- *
+ * @size: size of array
  * Return: character array always
  */
 char *create_array(unsigned int size, char c)
 {
-unsigned i = 0;
+unsigned int i = 0;
 char *arr = malloc(sizeof(char) * size);
 
 if (size == 0 || arr == NULL)
@@ -22,5 +22,5 @@ for (i = 0; i < size; i++)
 arr[i] = c;
 }
 
-return arr;
+return (arr);
 }
