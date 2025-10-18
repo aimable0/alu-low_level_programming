9ce2514d975b0bfe511ed36ae79dd8b407bb0123
Nkurikiyimana Aimable <aimablenkurikiyimana1@gmail.com>
2025-10-18 22:54:37 +0200

    conforming to betty

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "main.h"
 
 void free_grid(int **grid, int height)
 {
-    int i = 0;
-    for (i = 0; i < height; i++)
-    {
-        free(grid[i]);
-    }
-    free(grid);
+int i = 0;
+for (i = 0; i < height; i++)
+{
+free(grid[i]);
+}
+free(grid);
 }
