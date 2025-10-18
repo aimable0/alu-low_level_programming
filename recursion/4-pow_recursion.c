965d73f49cdb22a06d2b9718324c19e296587189
Nkurikiyimana Aimable <aimablenkurikiyimana1@gmail.com>
2025-10-18 13:26:19 +0200

    pow_recursion

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -11,24 +11,15 @@ int _pow_recursion(int x, int y);
  * Return: value of x raised to the power of y.
  */
 
-int main(void)
-{
-    int x = 2;
-    int y = 2;
-    int ans = _pow_recursion(x, y);
-    printf("pow: %d\n", ans);
-}
-
 int _pow_recursion(int x, int y)
 {
-    if (y == 0)
-    {
-        return 1;
-    }
-    else if (y == 1)
-    {
-        return x;
-    }
-
-    return 1 + _pow_recursion(x, --y);
+if (y < 0)
+{
+return (-1);
+}
+if (y == 0)
+{
+return (1);
+}
+return (x * _pow_recursion(x, --y));
 }
--- test.c
+++ test.c
@@ -1,31 +1,14 @@
 #include <stdio.h>
 
-int main(void)
+int _pow_recursion(int x, int y)
 {
-
-    for (int i = 0; i < 10; i++) {
-        for (int m = 0; m < 10; m++) {
-            if (m == 0) {
-                putchar('0');
-                continue;
-            }
-            if (i * m < 10) {
-                putchar(',');
-                putchar(' ');
-                putchar(' ');
-                putchar((i * m) + '0');
-            } else {
-                int product = i * m;
-                int last_digit = product % 10;
-                int first_digit = (product  - last_digit) / 10;
-                putchar(',');
-                putchar(' ');
-                putchar(first_digit + '0');
-                putchar(last_digit + '0');
-
-            }
-        }
-        printf("\n");
-    }
-    return 0;
+if (y < 0)
+{
+return (-1);
+}
+if (y == 0)
+{
+return (1);
+}
+return (x * _pow_recursion(x, --y));
 }
