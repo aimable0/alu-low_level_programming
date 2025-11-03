734b750dbb17bf12edbad7dd33525d877d4f0a54
aimable0 <aimablenkurikiyimana1@gmail.com>
2025-11-03 14:35:26 +0200

    005

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,11 +1,13 @@
+#include <stdlib.h>
+
 /**
  * dog - a new type for dog representation
  */
 typedef struct dog
 {
-    char *name;
-    float age;
     char *owner;
+    float age;
+    char *name;
 } dog;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
