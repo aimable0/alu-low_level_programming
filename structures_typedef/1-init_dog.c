355c89cb947d6504c9e60e34878dad4e0541dc0a
aimable0 <aimablenkurikiyimana1@gmail.com>
2025-11-03 14:55:22 +0200

    011

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -10,22 +10,7 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-    int i = 0;
-    /* copy */
-    while (name[i] != '\0')
-    {
-        d->name[i] = name[i];
-        i++;
-    }
-    d->name[i] = '\0';
-
-    i = 0; /* reset i for a fresch copy*/
-    while (owner[i] != '\0')
-    {
-        d->owner[i] = owner[i];
-        i++;
-    }
-    d->owner[i] = '\0';
-
+    d->name = name;
+    d->owner = owner;
     d->age = age;
 }
