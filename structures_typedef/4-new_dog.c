42cb470ccd86fd32a236bcfd0341320e70bce79a
aimable0 <aimablenkurikiyimana1@gmail.com>
2025-11-03 19:47:42 +0200

    02

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -39,7 +39,7 @@ dog_t *new_dog(char *name, float age, char *owner)
     if (new_dog == NULL)
         return NULL;
 
-    if (!name || !owner)
+    if (name == NULL || owner == NULL)
     {
         free(new_dog);
         return NULL;
