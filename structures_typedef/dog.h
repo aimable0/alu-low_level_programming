9c14ffe8bf410db7b5a29cc81f76d30d3af061e6
aimable0 <aimablenkurikiyimana1@gmail.com>
2025-11-03 15:19:07 +0200

    defining a new type

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,14 +1,14 @@
-#include <stdlib.h>
 
 /**
  * dog - a new type for dog representation
  */
-typedef struct dog
+struct dog
 {
     char *owner;
     char *name;
     float age;
-} dog;
+};
 
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
