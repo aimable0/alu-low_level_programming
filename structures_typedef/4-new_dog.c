ef0741bc91f0c4a39803939055eabad37d5bc4bd
aimable0 <aimablenkurikiyimana1@gmail.com>
2025-11-03 15:55:17 +0200

    trial 009

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -8,42 +8,44 @@ dog_t *new_dog(char *name, float age, char *owner)
     dog_t *new_dog = &dog;
     int i = 0;
 
-    /* allocate memory */
+    /* allocate memory for name */
     while (name[i] != '\0')
         i++;
-    dog.name = malloc(sizeof(char) * i);
+    new_dog->name = malloc(sizeof(char) * i);
 
+    /* allocate memory for owner */
     i = 0;
-    while (name[i] != '\0')
+    while (owner[i] != '\0')
         i++;
-    dog.owner = malloc(sizeof(char) * i);
+    new_dog->owner = malloc(sizeof(char) * i);
 
-    if (dog.name != NULL && name != NULL && owner != NULL && dog.owner != NULL)
+    /* check for non NULL*/
+    if (new_dog->name != NULL && new_dog->owner != NULL)
     {
-        dog.age = age;
+        new_dog->age = age;
 
         /* copy name */
         i = 0;
         while (name[i] != '\0')
         {
-            dog.name[i] = name[i];
+            new_dog->name[i] = name[i];
             i++;
         }
-        dog.name[i] = '\0';
+        new_dog->name[i] = '\0';
 
         /* copy owner & reset i */
         i = 0;
         while (owner[i] != '\0')
         {
-            dog.owner[i] = owner[i];
+            new_dog->owner[i] = owner[i];
             i++;
         }
-        dog.owner[i] = '\0';
+        new_dog->owner[i] = '\0';
     }
     else
     {
-        free(dog.name);
-        free(dog.owner);
+        free(new_dog->name);
+        free(new_dog->owner);
         return NULL;
     }
 
