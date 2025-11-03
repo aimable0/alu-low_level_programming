101a1508436f8702aedffa2a0254a64b71171c90
aimable0 <aimablenkurikiyimana1@gmail.com>
2025-11-03 15:40:58 +0200

    trial 003

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -3,37 +3,37 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-    dog_t *dog;
+    dog_t dog;
     int i = 0;
-    dog->name = malloc(sizeof(char) * 50);
-    dog->owner = malloc(sizeof(char) * 50);
+    dog.name = malloc(sizeof(char) * 50);
+    dog.owner = malloc(sizeof(char) * 50);
 
-    if (dog->name != NULL, name != NULL && owner != NULL, dog->owner != NULL)
+    if (dog.name != NULL, name != NULL && owner != NULL, dog.owner != NULL)
     {
-        dog->age = age;
+        dog.age = age;
 
         /* copy name */
         while (name[i] != '\0')
         {
-            dog->name[i] = name[i];
+            dog.name[i] = name[i];
             i++;
         }
-        dog->name[i] = '\0';
+        dog.name[i] = '\0';
 
         /* copy owner & reset i */
         i = 0;
         while (owner[i] != '\0')
         {
-            dog->owner[i] = owner[i];
+            dog.owner[i] = owner[i];
             i++;
         }
-        dog->owner[i] = '\0';
+        dog.owner[i] = '\0';
     }
     else
     {
-        free(dog->name);
-        free(dog->owner);
+        free(dog.name);
+        free(dog.owner);
         return NULL;
     }
-    return dog;
+    return &dog;
 }
