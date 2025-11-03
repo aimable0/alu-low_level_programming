0697ca758b6e2ac78003247bbd268b6d5a1c2641
aimable0 <aimablenkurikiyimana1@gmail.com>
2025-11-03 21:06:18 +0200

    . .

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -14,9 +14,10 @@ char *owner;
 char *name;
 float age;
 } dog_t;
-#endif /* DOG_H*/
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+#endif /* DOG_H*/
+
