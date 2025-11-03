d7f4db197c28a1e2f949aa8f0d1464271c57d4e7
aimable0 <aimablenkurikiyimana1@gmail.com>
2025-11-03 20:22:28 +0200

    ada

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -4,9 +4,10 @@
 
 void free_dog(dog_t *d)
 {
-    if (d) free(d);
-    else return;
-
-    if (d->name) free(d->name);
-    if (d->owner != NULL) free(d->owner);
+    if (d)
+        free(d);
+    else
+        return;
+    free(d->name);
+    free(d->owner);
 }
