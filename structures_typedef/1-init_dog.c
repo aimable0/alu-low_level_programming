3a337ee38e1a4bed485376a779a6995f169f569b
aimable0 <aimablenkurikiyimana1@gmail.com>
2025-11-03 13:28:43 +0200

    initiate dog

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "dog.h"
+#include <string.h>
+
+/**
+ * init_dog - initializes a variable of type struct dog
+ *!params here..
+ *
+ * Return: Nothing
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+    strcpy(d->name, *name);
+    d->age = age;
+    strcpy(d->owner, *owner);
+}
