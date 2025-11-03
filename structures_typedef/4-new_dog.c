db60fbb2dbbb9cb95551e33df0d59558ef197cc4
aimable0 <aimablenkurikiyimana1@gmail.com>
2025-11-03 19:38:52 +0200

    11

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -42,7 +42,14 @@ dog_t *new_dog(char *name, float age, char *owner)
     new_dog->age = age;
 
     new_dog->name = malloc(sizeof(char) * (_strlen(name) + 1));
-    _strcpy(new_dog->name, name);
+    if (new_dog->name == NULL)
+    {
+        free(new_dog);
+        return NULL;
+    }
+
+    if (new_dog->name)
+        _strcpy(new_dog->name, name);
 
     new_dog->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
     _strcpy(new_dog->owner, owner);
