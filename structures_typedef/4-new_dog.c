415eae99a6740d15e5a883d525cf7f87c0f21486
aimable0 <aimablenkurikiyimana1@gmail.com>
2025-11-03 18:27:18 +0200

    b

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -3,16 +3,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(void)
-{
-    dog_t *my_dog;
-
-    my_dog = new_dog("Ghost", 4.75, "Jon Snow");
-
-    printf("My name is %s, and I am %.1f :) with my owner %s - Woof!\n", my_dog->name, my_dog->age, my_dog->owner);
-    return (0);
-}
-
 void _strcpy(char *dest, char *src)
 {
     int i = 0;
@@ -45,8 +35,6 @@ dog_t *new_dog(char *name, float age, char *owner)
     }
 
     _strcpy(dog.name, name);
-
-
 
     _strcpy(dog.owner, owner);
 
--- structures_typedef/test_newdog.c
+++ structures_typedef/test_newdog.c
@@ -2,54 +2,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-dog_t *new_dog(char *name, float age, char *owner)
+int main(void)
 {
-    dog_t dog;
-    dog_t *new_dog = &dog;
-    int i = 0;
+    dog_t *my_dog;
 
-    /* allocate memory for name */
-    while (name[i] != '\0')
-        i++;
-    new_dog->name = malloc(sizeof(char) * i);
+    my_dog = new_dog("Ghost", 4.75, "Jon Snow");
 
-    /* allocate memory for owner */
-    i = 0;
-    while (owner[i] != '\0')
-        i++;
-    new_dog->owner = malloc(sizeof(char) * i);
-
-    /* check for non NULL*/
-    if (new_dog->name != NULL && new_dog->owner != NULL)
-    {
-        new_dog->age = age;
-
-        /* copy name */
-        i = 0;
-        while (name[i] != '\0')
-        {
-            new_dog->name[i] = name[i];
-            i++;
-        }
-        new_dog->name[i] = '\0';
-
-        /* copy owner & reset i */
-        i = 0;
-        while (owner[i] != '\0')
-        {
-            new_dog->owner[i] = owner[i];
-            i++;
-        }
-        new_dog->owner[i] = '\0';
-    }
-    else
-    {
-        free(new_dog->name);
-        free(new_dog->owner);
-        return NULL;
-    }
-
-    return new_dog;
+    printf("My name is %s, and I am %.1f :) with my owner %s - Woof!\n", my_dog->name, my_dog->age, my_dog->owner);
+    return (0);
 }
 
 
