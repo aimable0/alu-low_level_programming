cd4bf6982ce2661d352dcb108aab1a033b0896c8
aimable0 <aimablenkurikiyimana1@gmail.com>
2025-11-12 16:19:53 +0200

    .

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -2,15 +2,16 @@
 
 /**
  * print_name - prints a name using function pointer
- * @name: name to print on screen
- * @f: function pointer
+ * @name: name to print on stdout
+ * @f: function pointer that prints the name
  *
  * Return: Nothing
  */
 void print_name(char *name, void (*f)(char *))
 {
-    if (f == NULL) {
-        return;
-    }
-    f(name);
+if (f == NULL)
+{
+return;
+}
+f(name);
 }
--- function_pointers/3-calc.h
+++ function_pointers/3-calc.h
@@ -1,9 +1,12 @@
- int op_add(int a, int b);
- int op_sub(int a, int b);
- int op_mul(int a, int b);
- int op_div(int a, int b);
- int op_mod(int a, int b);
- int (*get_op_func(char *s))(int, int);
+#ifndef CALC_H
+#define CALC_H
+
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+int (*get_op_func(char *s))(int, int);
 
 
 /**
@@ -14,6 +17,8 @@
  */
 typedef struct op
 {
-    char *op;
-    int (*f)(int a, int b);
+char *op;
+int (*f)(int a, int b);
 } op_t;
+
+#endif
