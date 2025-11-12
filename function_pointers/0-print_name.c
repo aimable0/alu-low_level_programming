f38e41bedc48c294df3dff0a1e424291e344ef29
aimable0 <aimablenkurikiyimana1@gmail.com>
2025-11-12 16:08:45 +0200

    001 - conforming to betty style

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -4,10 +4,13 @@
  * print_name - prints a name using function pointer
  * @name: name to print on screen
  * @f: function pointer
- * 
+ *
  * Return: Nothing
  */
 void print_name(char *name, void (*f)(char *))
 {
+    if (f == NULL) {
+        return;
+    }
     f(name);
 }
