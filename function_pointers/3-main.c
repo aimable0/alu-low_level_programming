4bbd163883a86b9f2ed75cc22d85c3f2262c299a
aimable0 <aimablenkurikiyimana1@gmail.com>
2025-11-12 16:06:13 +0200

    more refines to the 3-main.c code structure

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -16,16 +16,22 @@ int main(int argc, char *argv[])
         return 98;
     }
 
-    operand_1 = atoi(argv[1]);
-    operand_2 = atoi(argv[3]);
     operator = argv[2];
-
     operation = get_op_func(operator);
     if (operation == NULL)
     {
         printf("Error\n");
         return 99;
     }
+
+    operand_1 = atoi(argv[1]);
+    operand_2 = atoi(argv[3]);
+    if (operand_2 == 0)
+    {
+        printf("Error\n");
+        return 100;
+    }
+
     result = operation(operand_1, operand_2);
     printf("%d\n", result);
 
