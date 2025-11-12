2d672410b9eb0e5d47f3f0ca9e02259c2b784ac9
aimable0 <aimablenkurikiyimana1@gmail.com>
2025-11-12 16:28:31 +0200

    more refinement.s on 01

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -2,25 +2,28 @@
 
 /**
  * int_index - a function that searches for an integer.
- * @array: ...
- * @size: ...
- * @cmp: ...
+ * @array: array that contains integers
+ * @size: length of the array
+ * @cmp: pointer to a function that confirms if the integer is found
  *
  * Return: Nothing.
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-    int i = 0;
-    if (size <= 0)
-    {
-        return (-1);
-    }
+int i = 0;
+if (size <= 0)
+{
+return (-1);
+}
+if (array == NULL || cmp == NULL) {
+return (-1);
+}
 
-    for (; i < size; i++)
-    {
-        if (cmp(array[i]) != 0) {
-            return i;
-        };
-    }
-    return (-1);
+for (; i < size; i++)
+{
+if (cmp(array[i]) != 0) {
+return i;
+};
+}
+return (-1);
 }
